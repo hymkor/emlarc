63c0b477852abce8ab850b5f5783c204953bdb62
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-03-25 18:00:49 +0900

    Add headers

--- main.go
+++ main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"crypto/rand"
 	"encoding/base64"
 	"fmt"
+	"io"
 	"mime/multipart"
+	"net/textproto"
 	"os"
 )
 
+func makeBoundString() (string, error) {
+	var buf [30]byte
+	_, err := io.ReadFull(rand.Reader, buf[:])
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", buf[:]), nil
+}
+
 func mains(args []string) error {
+	bound, err := makeBoundString()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Content-Type: multipart/mixed; boundary=\"%s\"\n\n", bound)
+
 	w := multipart.NewWriter(os.Stdout)
 	defer w.Close()
+	w.SetBoundary(bound)
+
 	for _, arg := range args {
-		w1, err := w.CreateFormFile(arg, arg)
+		header := textproto.MIMEHeader{}
+
+		header.Add("Content-Disposition",
+			fmt.Sprintf(`form-data; name="%[1]s"; filename="%[1]s"`, arg))
+		header.Add("Content-Type", "application/octet-stream")
+		header.Add("Content-Transfer-Encoding", "base64")
+
+		w1, err := w.CreatePart(header)
 		if err != nil {
 			return fmt.Errorf("%s: %w", arg, err)
 		}
